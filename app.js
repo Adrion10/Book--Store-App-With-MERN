b95bf3931fc0f5967bba6173b6d28b17a1ea642c
Adrion10 <lacehila@gmail.com>
2022-07-06 23:31:32 +0200

    connect to database

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-mongoose.connect("mongodb://localhost:27017/BookStore").then(() => {
-  console.log("Connected to Database");
-});
+mongoose
+  .connect("mongodb://localhost:27017/BookStore")
+  .then(() => console.log("Connected to Database"))
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => console.log(err));
