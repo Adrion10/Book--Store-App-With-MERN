2a1c25e8c6129d0020ef278ca43f4039a5aa31d4
Adrion10 <lacehila@gmail.com>
2022-07-07 12:37:48 +0200

    bookmodel

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+//middlewares
+
+app.use("/", (req, res, next) => {
+  res.send("this is our starting app");
+});
 mongoose
   .connect("mongodb://localhost:27017/BookStore")
   .then(() => console.log("Connected to Database"))
---
+++ model/BookModel.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+  },
+});
